9658bfbc49759abee8abee821e1116a7b825b9ab
enoquetembe <enoquetembetembe10@gmail.com>
2023-07-17 21:36:21 -0300

    adding Button component

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,48 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Button, ButtonProps } from '@enoque/react'
+import { ArrowRight } from 'phosphor-react'
+
+export default {
+  title: 'Form/Button',
+  component: Button,
+  args: {
+    children: 'Send',
+  },
+} as Meta<ButtonProps>
+
+export const Primary: StoryObj<ButtonProps> = {}
+
+export const Secondary: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'secondary',
+  },
+}
+
+export const Tertiary: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'tertiary',
+    children: 'Cancel',
+  },
+}
+
+export const Small: StoryObj<ButtonProps> = {
+  args: {
+    size: 'sm',
+  },
+}
+
+export const Disabled: StoryObj<ButtonProps> = {
+  args: {
+    disabled: true,
+  },
+}
+
+export const WithIcon: StoryObj<ButtonProps> = {
+  args: {
+    children: (
+      <>
+        Next Step <ArrowRight weight="bold" />
+      </>
+    ),
+  },
+}
---
+++ packages/react/src/components/Button.tsx
@@ -0,0 +1,93 @@
+import { ComponentProps, ElementType } from 'react'
+import { styled } from '../styles'
+
+export const Button = styled('button', {
+  all: 'unset',
+  cursor: 'pointer',
+  '&:disabled:hover': {
+    cursor: 'not-allowed',
+  },
+
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  gap: '$2',
+  padding: '0, $4',
+
+  minWidth: 120,
+  boxSizing: 'border-box',
+  borderRadius: '$sm',
+
+  fontSize: '$sm',
+  fontWeight: '$medium',
+  fontFamily: '$default',
+
+  svg: {
+    width: '$4',
+    height: '$4',
+  },
+
+  variants: {
+    variant: {
+      primary: {
+        backgroundColor: '$green500',
+        color: '$white',
+
+        '&:not(disabled):hover': {
+          backgroundColor: '$green300',
+        },
+
+        '&:disabled:hover': {
+          backgroundColor: '$gray200',
+        },
+      },
+
+      secondary: {
+        color: '$green300',
+        border: '2px solid $green500',
+
+        '&:not(disabled):hover': {
+          backgroundColor: '$green500',
+          color: '$white',
+        },
+
+        '&:disabled:hover': {
+          color: '$gray200',
+          borderColor: '$gray200',
+        },
+      },
+
+      tertiary: {
+        color: '$gray100',
+
+        '&:not(disabled):hover': {
+          color: '$white',
+        },
+
+        '&:disabled:hover': {
+          color: '$gray600',
+        },
+      },
+    },
+
+    size: {
+      sm: {
+        height: 38,
+      },
+
+      md: {
+        height: 46,
+        padding: '0, $4',
+      },
+    },
+  },
+
+  defaultVariants: {
+    variant: 'primary',
+    size: 'md',
+  },
+})
+
+export interface ButtonProps extends ComponentProps<typeof Button> {
+  as?: ElementType
+}
