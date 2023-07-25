1d7e66a12a685124e44cd05ad08f20c1bda21ea2
enoquetembe <enoquetembetembe10@gmail.com>
2023-07-25 20:07:06 -0300

    fix: adding storybook configs

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,26 +1,26 @@
-/** @type { import('@storybook/react-vite').StorybookConfig } */
-const config = {
-  stories: ["../src/pages/**/*.mdx", "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
+module.exports = {
+  "stories": [
+    "../src/pages/**/*.stories.mdx",
+    "../src/stories/**/*.stories.tsx"
+  ],
+  "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-a11y"
   ],
-  framework: {
-    name: "@storybook/react-vite",
-    options: {},
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-vite"
   },
-  docs: {
-    autodocs: "tag",
+  "features": {
+    "storyStoreV7": true
   },
-
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/design-system/'
+      config.base = '/05-design-system/'
     }
 
     return config
   }
-};
-export default config;
+}
