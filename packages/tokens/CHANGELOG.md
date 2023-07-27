cf81738bc31120ac90e4c06a6d95653d8779adb1
enoquetembe <enoquetembetembe10@gmail.com>
2023-07-27 13:25:04 -0300

    chore: add new color to tokens

--- packages/tokens/CHANGELOG.md
+++ packages/tokens/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @enoque-ui/tokens
 
+## 2.1.0
+
+### Minor Changes
+
+- add orange color
+
 ## 2.0.0
 
 ### Major Changes
--- packages/tokens/src/colors.ts
+++ packages/tokens/src/colors.ts
@@ -2,6 +2,7 @@ export const colors = {
   white: '#FFF',
   black: '#000',
   red: '#E25858',
+  orange: '#FF9000',
 
   gray100: '#E1E1E6',
   gray200: '#A9A9B2',
